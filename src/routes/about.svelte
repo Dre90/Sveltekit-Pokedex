bb7c87fd39ce3a894e09723268a6793ae93911ce
Dag-Roger Eriksen <dag.roger.e@gmail.com>
2021-08-15 21:32:01 +0200

    Pokedex

---
+++ src/routes/about.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<title>About Svelte Kit Pokedex</title>
+</svelte:head>
+
+<h1 class="text-4xl text-center my-8 uppercase">About</h1>
+<p>This is a relly cool Svelte kit demo using Pokemon API to build a Pokedex.</p>
